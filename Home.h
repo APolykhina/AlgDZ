c9115bbc204b987115a0aff9eb28bd2ce4d808f5
Nastya232323 <nastya23232@mail.ru>
2016-05-09 00:34:42 +0400

    Добавлено сохранение и восстановление данных

--- Home.h
+++ Home.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 #include <vector>
 using namespace std;
 
@@ -7,20 +8,21 @@ using namespace std;
 #define Price_of_Electricity 40
 
 struct GKH {
-	bool HotWater;
-	bool CouldWater;
-	bool Electricity;
+	int HotWater;
+	int CouldWater;
+	int Electricity;
 };
 
 class Home
 {
 public:
+	friend ostream& operator<<(ostream& , const Home&);
 	friend class System;
 	int Schet();
 	int GetNumberofHome();
 	Home();
 	Home(int, int,GKH);
-	void operator()(int, int, GKH);
+	void operator()(int, int, GKH,int);
 	~Home() {};
 private:
 	int Dolg;
@@ -28,6 +30,17 @@ private:
 	int MenCount;
 	GKH gkh;
 };
+
+ostream& operator<<(ostream& os, const Home& H) {
+
+	os << "Number " << H.IHome << endl;
+	os << "CountMen " << H.MenCount << endl;
+	os << "HotWater " << H.gkh.HotWater << endl;
+    os << "CouldWater " << H.gkh.CouldWater << endl; 
+	os << "Electicity " << H.gkh.Electricity <<endl ;
+	os <<"Dolg " << H.Dolg <<endl ;
+	return os;
+}
 
 int Home::Schet() {
 	int sum =0;
@@ -47,10 +60,11 @@ int Home::GetNumberofHome() {
 	return IHome;
 }
 
-void Home::operator()(int I, int M,GKH G) {
+void Home::operator()(int I, int M,GKH G,int D) {
 	IHome = I;
 	MenCount = M;
 	gkh = G;
+	Dolg = D;
 }
 
 Home::Home() {
@@ -66,5 +80,7 @@ Home::Home(int I, int M,GKH G) {
 	gkh = G;
 	Dolg = 0;
 }
+
+
 
 
